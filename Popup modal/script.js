d025b902b3b0a10e935fdd1de311334517522813
CodeTraversal <122677596+CodeTraversal@users.noreply.github.com>
2023-03-20 19:27:00 +0530

    Delete Popup modal directory

--- Popup modal/script.js
+++
@@ -1,9 +0,0 @@
-let popup = document.querySelector('.popup');
-
-let openModal = () => {
-    popup.classList.add('popup_open');
-}
-
-let closeModal = () =>{
-    popup.classList.remove('popup_open');
-}
