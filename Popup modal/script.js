243a2e28463dc5aec816bb897795a593aea212a6
CodeTraversal <122677596+CodeTraversal@users.noreply.github.com>
2023-03-20 19:21:47 +0530

    Add files via upload

---
+++ Popup modal/script.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector('.popup');
+
+let openModal = () => {
+    popup.classList.add('popup_open');
+}
+
+let closeModal = () =>{
+    popup.classList.remove('popup_open');
+}
